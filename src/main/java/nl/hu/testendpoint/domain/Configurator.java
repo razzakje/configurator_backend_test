bc93043992f5870fc4de7071741875d63046fabb
Day1c <dewancheng@cheng.com>
2025-04-07 13:34:53 +0200

    DAO added

--- src/main/java/nl/hu/testendpoint/domain/Configurator.java
+++ src/main/java/nl/hu/testendpoint/domain/Configurator.java
@@ -1,4 +1,5 @@
 package nl.hu.testendpoint.domain;
+import nl.hu.testendpoint.persistence.ComponentDAO;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,20 +14,26 @@ public class Configurator {
         this.name = name;
         this.components = components;
     }
-
-    static public Configurator getConfigurator() {
-        List<Component> components = new ArrayList<>();
-        components.add(new Component("cpu", "intel core i9", 200.40));
-        components.add(new Component("cpu cooler", "shark cooler", 60.40));
-        components.add(new Component("gpu", "nvidia 5070", 500.99) {});
-        components.add(new Component("motherboard", "b550", 15) {});
-        components.add(new Component("memory", "Intel i9", 10) {});
-        components.add(new Component("storage", "Intel i9", 10) {});
-        components.add(new Component("case", "Intel i9", 100) {});
-        components.add(new Component("psu", "Intel i9", 10) {});
-        components.add(new Component("os", "Intel i9", 10) {});
-        return new Configurator(1,"dewan", components);
+    public static Configurator getConfigurator() {
+        ComponentDAO dao = new ComponentDAO();
+        List<Component> components = dao.findAll();
+        return new Configurator(1, "dewan", components);
     }
+
+
+//    static public Configurator getConfigurator() {
+//        List<Component> components = new ArrayList<>();
+//        components.add(new Component("cpu", "intel core i9", 200.40));
+//        components.add(new Component("cpu cooler", "shark cooler", 60.40));
+//        components.add(new Component("gpu", "nvidia 5070", 500.99) {});
+//        components.add(new Component("motherboard", "b550", 15) {});
+//        components.add(new Component("memory", "Intel i9", 10) {});
+//        components.add(new Component("storage", "Intel i9", 10) {});
+//        components.add(new Component("case", "Intel i9", 100) {});
+//        components.add(new Component("psu", "Intel i9", 10) {});
+//        components.add(new Component("os", "Intel i9", 10) {});
+//        return new Configurator(1,"dewan", components);
+//    }
 
     public int getId() {
         return id;
